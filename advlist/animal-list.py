355e36390ec01da1d671a53c143867a42bbf8796
GitHub-Ros <thamothy.ros@gmail.com>
2023-11-07 20:30:12 +0000

    learning about dictionaries

--- advlist/animal-list.py
+++ advlist/animal-list.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
-animal_list = ('Fox', 'Fly', 'Ant', 'Bee', 'Cod', 'Dog', 'Yak', 'Cow', 'Hen', 'Koi', 'Hog', 'Jay', 'Kit')
+animal_list = ("Fox", "Fly", "Ant", "Bee", "Cod", "Dog", 'Yak', 'Cow', 'Hen', 'Koi', 'Hog', 'Jay', 'Kit')
 print(animal_list)
