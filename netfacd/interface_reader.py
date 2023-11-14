eebea5fad618bdb297eb6995481ab2726a453e40
GitHub-Ros <thamothy.ros@gmail.com>
2023-11-14 17:11:11 +0000

    Python and physical interfaces

--- netfacd/interface_reader.py
+++ netfacd/interface_reader.py
@@ -3,14 +3,28 @@
 
 import netifaces
 
-print(netifaces.interfaces())
+def find_mac(interface_name):
+    """passed interface name (string), returns the MAC of that interface (string)"""
+    return (netifaces.ifaddresses(interface_name)[netifaces.AF_LINK])[0]['addr']
 
-for i in netifaces.interfaces():
+def find_ip(interface_name):
+    """passed interface name (string), returns the IP of that interface (string)"""
+    return (netifaces.ifaddresses(interface_name)[netifaces.AF_INET])[0]['addr']
+
+def main():
+    """runtime"""
+
+    print(netifaces.interfaces())
+
+    for i in netifaces.interfaces():
         print('\n**************Details of Interface - ' + i + ' *********************')
         try: # This is a new line, you also need to indent the code below this line by 4 whitespaces
-            print('MAC: ', end='') # This print statment will always print MAC without an end of line
-            print(netifaces.ifaddresses(i)[netifaces.AF_LINK][0]['addr']) # Prints the MAC address
-            print('IP: ', end='') # This print statement will always print IP without an end of line
-            print(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr']) # Prints the IP address
+            print('MAC: ', find_mac(i)) # This print statment will always print MAC without an end of line
+
+
+            print('IP: ', find_ip(i)) # This print statement will always print IP without an end of line
         except: # This is a new line
             print('Could not collect adapter information') # Print an error message
+
+if __name__ == "__main__":
+    main()
